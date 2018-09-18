1882c9bcd3db3610d473e1accd9d682a4e1e9051
Cheddi Charles <cheddi.charles@gmail.com>
2018-09-18 02:12:03 -0500

    working on  interval timing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ var countDown = 10;
 var isCountDownEnded = false;
 var resetTime;
 var choices
+var totalQuestions = 0;
 
 var Trivia = [
     {
@@ -44,120 +45,111 @@ var Trivia = [
 function display(count) {
     //   console.log(Trivia[count].answerChoices);
     console.log(Trivia[count].q);
-    
+
     var questions = $("<h3>");
     questions.text(Trivia[count].q);
     $("#questions ").append(questions);
     for (var j = 0; j < Trivia[count].answerChoices.length; j++) {
-      choices = $("<button>").text(Trivia[count].answerChoices[j]);
+        choices = $("<button>").text(Trivia[count].answerChoices[j]);
         $("#choices").append(choices);
         console.log(Trivia[count].answerChoices[j])
-   
-    }      
-   $("#choices button").on("click",function(){
+
+    }
+
+    $("#choices button").on("click", function () {
         console.log($(this).text());
         console.log(Trivia[count].a);
 
-          var x = Trivia[count].a; 
-          var y = $(this).text();
-        if(x ===y){
-        console.log("you are a winner");
-        $("#questions ").empty();
-        $("#choices").empty();
-          setTimeout(nextQuestion,2000);  
-         }  
-    
-})
-    
+
+        var x = Trivia[count].a;
+        var y = $(this).text();
+        if ((countDown !== 0) || (countDown > 0)) {
+            if (x === y) {
+                console.log("you are a winner");
+                $("#outcome").append("Congratulations , You got it right");
+
+                $("#questions ").empty();
+                $("#choices").empty();
+                reset();
+                setTimeout(nextQuestion, 2000);
+            }
+            if (x !== y) {
+                $("#outcome").append("Sorry, you got it worng. The correct anwser is " + x);
+                $("#questions ").empty();
+                $("#choices").empty();
+                reset();
+                setTimeout(nextQuestion, 2000);
+            }
+
+        }
+
+    })
+
 }
 
 
- 
+
 // reset time
-function reset(){
-    countDown=10;   
+function reset() {
+    countDown = 10;
 }
 
 // decrement function to countdown time 
 function decrement() {
-   
+
     if (!isCountDownEnded) {
         countDown--
         $("#countDown").text(countDown)
-        if(countDown===0){ 
-         count++;
-         isCountDownEnded=true
-   
+        if (countDown === 0) {
+            //  count++;
+            $("#outcome").append(" Out of Time. The correct anwser is " + Trivia[count].a);
+            $("#questions ").empty();
+            $("#choices").empty();
+            reset();
+            setTimeout(nextQuestion, 2000);
+
         }
-    
-        //  setInterval ( nextQuestion,5000);
+
+
     }
+    // if(totalQuestions===Trivia.length){
+    //     $("#outcome").append(" You have come to the end. There are no more questions")
+    //     isCountDownEnded=true
+    // }
 
 }
 
-function start(){
-       display(count);
-        if(!isCountDownEnded){
+function start() {
+    display(count);
+    if (!isCountDownEnded) {
         setInterval(decrement, 1000);
 
-       
-       } 
-    
+    }
+
 }
 
 // start the game by clicking start button
-$(".button").click(start) 
+$(".button").click(start)
 
 
 // function to display next question
-var nextQuestion= function() {
-       count++;
-        reset();
-        display(count);      
-        
-    }
-
+var nextQuestion = function () {
+    count++;
+    totalQuestions++
+    reset();
+    display(count);
+    $("#outcome").empty();
    
 
 
-
-
-
-
-// for(var i=0;i<Trivia.length;i++){
-
-//    var questions = $("<h3>");
-
-//     questions.text(Trivia[i].q);
-// $(".questions div").append(questions);
-
-
-// for(var j=0;j<Trivia[i].answerChoices.length;j++){
-//     var choices =$("<span>").text(Trivia[j].answerChoices[j]);
-//     var input =$('<input type="radio" name="radio_name">')
-
-//     $(".questions div").append(input,choices);
-// }
-
-
-// }
+}
 
 
 
 
 
-// var questionsArray = [
-// "Which country won the last FIFA world cup?",
-// "Which team won the 2017-2018 UEFA Champion League?",
-// "Which NBA team won the 2017-2018 finals?",
-// "Which team won the 2018 Super Bowl?",
-// "How many seconds are there in 1 hour?"]
 
-// var answerChoices=[
-// {q1 : ["England","Belgium","France","Brazil"]},
-// {q2:["Manchester United","Real Madrid","Barcelona", "Bayern Munich"]},
-// {q3:["Warriors","Rockets", "Calviers","Celtics"]},
-// {q4: ["Cowboys","Packers","Eagles","Panthers"]},
-// {q5: ["3600","365","5000","860"]}
 
-// 
+
+
+
