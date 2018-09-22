656f9390612217e86144d187dee25e6b14203078
Cheddi Charles <cheddi.charles@gmail.com>
2018-09-21 20:35:20 -0500

    final changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,6 +13,7 @@ var totalQuestions = 0;
 var clickStart = false;
 var gameEnded = false;
 
+// array to hold the data of questions, options and answers
 var Trivia = [
     {
         q: "Which country won the last FIFA world cup?",
@@ -45,13 +46,15 @@ var Trivia = [
         answerChoices: ["3600", "365", "5000", "860"]
     },
     {
-        q: "What is the probabilty of getting a heart suit ia 52 deck of cards?",
+        q: "What is the probabilty of getting a heart suit in a 52 deck of cards?",
         a: "1/4",
         answerChoices: ["1/2", "2/5", "1/13", "1/4"]
     }
 ]
 
+// variable to hold the setinterval for the countdown of time
 var InterValid;
+
 // create function to display questions and answer choices
 function display(count) {
     totalQuestions++;
@@ -66,12 +69,12 @@ function display(count) {
     for (var j = 0; j < Trivia[count].answerChoices.length; j++) {
         choices = $("<button>").text(Trivia[count].answerChoices[j]);
         choices.addClass(" btn btn-info m-2");
-       
+
         $("#choices").append(choices);
         console.log(Trivia[count].answerChoices[j])
 
     }
-    
+
     // start the countdown when you click the start button
     InterValid = setInterval(decrement, 1000);
 
@@ -86,8 +89,8 @@ function display(count) {
             if (x === y) {
                 numCorrect++;
                 console.log("you are a winner");
-                var giphy = $("<img>").attr("src","https://static.raru.co.za/cover/2017/02/10/5488866-l.jpg?v=1486736058");
-                    giphy.css({"width":"100px","height":"100px"});
+                var giphy = $("<img>").attr("src", "https://static.raru.co.za/cover/2017/02/10/5488866-l.jpg?v=1486736058");
+                giphy.css({ "width": "100px", "height": "100px" });
                 $("#outcome").append(giphy);
                 $("#outcome").append("Congratulations , You got it right");
 
@@ -96,7 +99,7 @@ function display(count) {
                 resetTime();
                 clearInterval(InterValid);
                 if (totalQuestions === Trivia.length) {
-                //    $("#outcome").empty();
+                    //    $("#outcome").empty();
                     finishGame(totalQuestions);
 
                 }
@@ -106,9 +109,9 @@ function display(count) {
             }
             if (x !== y) {
                 numWrong++;
-                var sad = $("<img>").attr("src","https://lh5.googleusercontent.com/-3dJnvJSY4tU/TYO-hs_TpEI/AAAAAAAAAS0/OY3i6DSriRU/s1600/S01E14b-+Crazy+sad.png");
-                    sad.css({"width":"100px","height":"100px"});
-                    $("#outcome").append(sad);  
+                var sad = $("<img>").attr("src", "https://lh5.googleusercontent.com/-3dJnvJSY4tU/TYO-hs_TpEI/AAAAAAAAAS0/OY3i6DSriRU/s1600/S01E14b-+Crazy+sad.png");
+                sad.css({ "width": "100px", "height": "100px" });
+                $("#outcome").append(sad);
                 $("#outcome").append("Sorry, you got it wrong. The correct anwser is " + x);
                 $("#questions ").empty();
                 $("#choices").empty();
@@ -146,9 +149,9 @@ function decrement() {
         $("#countDown").text(countDown);
         if (countDown === 0) {
             numUnanswered++;
-            var timesUp = $("<img>").attr("src","http://www.successconnections.com/wp-content/uploads/2013/10/outoftime.jpg");
-             timesUp.css({"width":"100px","height":"100px"});
-              $("#outcome").append(timesUp);  
+            var timesUp = $("<img>").attr("src", "http://www.successconnections.com/wp-content/uploads/2013/10/outoftime.jpg");
+            timesUp.css({ "width": "100px", "height": "100px" });
+            $("#outcome").append(timesUp);
             $("#outcome").append("  The correct anwser is " + Trivia[count].a);
             $("#questions").empty();
             $("#choices").empty();
@@ -165,7 +168,7 @@ function decrement() {
 
 }
 
-
+//function to start the game when you click the start button
 function start() {
     if (!clickStart) {
         display(count);
@@ -188,15 +191,15 @@ function nextQuestion() {
     $("#outcome").empty();
 
 }
-
+ // function to show the results at the end of the game
 function finishGame(totalQuestions) {
-    var endOfGame =$("<img>").attr("src","https://gifer.com/i/3dnq.gif");
-       endOfGame.css({"width":"200px","height":"100px"});
+    var endOfGame = $("<img>").attr("src", "https://gifer.com/i/3dnq.gif");
+    endOfGame.css({ "width": "200px", "height": "100px" });
     var results = $("<h3>").append("Correct :  " + numCorrect);
     var incorrect = $("<h3>").append("Incorrect :  " + numWrong);
     var unattemped = $("<h3>").append("Unanswered :  " + numUnanswered);
     results.append(incorrect, unattemped);
-     $("#outcome").empty();
+    $("#outcome").empty();
     console.log(totalQuestions);
     console.log(Trivia.length);
     if (!gameEnded) {
